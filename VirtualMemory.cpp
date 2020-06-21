965c9cf9fbc6f11d990ded9aa7997f5101f7ace8
Avinoam <avinoam@hershler.com>
2020-06-21 17:33:53 +0300

    Added more documentation

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -67,15 +67,18 @@ uint64_t cyclicDistance(uint64_t pageIndex, uint64_t currentPageIndex)
 }
 
 /**
- * @brief Finds a free frame for a given page.
- * @param pageIndex
- * @param currentPageIndex
- * @param highestAddress
- * @param victim
- * @param ignoreFrame
- * @param frameAddress
- * @param depth
- * @return the address of the free frame, 0 if .
+ * @brief Finds a free frame for a given page, according to the priorities specified in the exercise
+ * description.
+ * @param pageIndex Index of the page we are finding a frame for (or we are findong a frame for a
+ * table that will index this page).
+ * @param currentPageIndex Current path we took through the tree to the current frame.
+ * @param highestAddress The highest address we have encountered so far.
+ * @param victim The best victim we have encountered so far, in case we need to evict.
+ * @param ignoreFrame A frame that may hold an empty table, but we should ignore that table because
+ * it is part of the path to the page we are currently accessing.
+ * @param frameAddress The address of the frame we are currently looking at.
+ * @param depth The depth we are at in the table tree. starts at 0 and goes up till TREE_DEPTH.
+ * @return the address of the free frame. shouldn't ever return 0.
  */
 uint64_t findFreeFrame(uint64_t pageIndex, uint64_t currentPageIndex = 0,
                        uint64_t &&highestAddress = 0,
@@ -85,43 +88,56 @@ uint64_t findFreeFrame(uint64_t pageIndex, uint64_t currentPageIndex = 0,
 
     if (frameAddress > highestAddress)
     {
+    	// If needed, update the highest address.
         highestAddress = frameAddress;
     }
     if (TABLES_DEPTH == depth)
     {
-        if (cyclicDistance(pageIndex, currentPageIndex >> OFFSET_WIDTH) >
+    	// we've reached a page, so check its cyclic distance from our page, and update victim if
+    	// this page is a better candidate.
+    	currentPageIndex = currentPageIndex >> OFFSET_WIDTH;
+        if (cyclicDistance(pageIndex, currentPageIndex) >
             cyclicDistance(pageIndex, victim))
         {
-            victim = currentPageIndex >> OFFSET_WIDTH;
+            victim = currentPageIndex;
         }
         return 0;
     }
     if (frameAddress != 0 && frameAddress != ignoreFrame && depth < TABLES_DEPTH &&
         isEmptyTable(frameAddress))
     {
+    	// We've found an empty table, so return its address as a free frame.
         return frameAddress;
     }
+    // Iterate through the entries in the current table and search for a free frame.
     for (size_t index = 0; index < PAGE_SIZE; ++index)
     {
         word_t address;
         PMread(frameAddress + index, &address);
         if (address)
         {
+        	// This entry pints somewhere, so continune searching.
             uint64_t result = findFreeFrame(pageIndex, (currentPageIndex + index) << OFFSET_WIDTH,
                                             std::move(highestAddress), std::move(victim),
                                             ignoreFrame, address * PAGE_SIZE, depth + 1);
             if (result)
             {
+            	// A table containing all zeros was found, so return it.
                 if (result == (unsigned long long) address * PAGE_SIZE)
                 {
+                	// The frame we just returned from was an empty table, so remove it from this
+                	// table.
                     PMwrite(frameAddress + index, 0);
                 }
                 return result;
             }
+            // Nothing found here, keep searching.
         }
     }
     if (depth == 0)
     {
+    	// We've iterated through the entire tree and no free frame was found, so either find the
+    	// next unused frame or evict a page.
         if (highestAddress + PAGE_SIZE < NUM_FRAMES * PAGE_SIZE)
         {
             return highestAddress + PAGE_SIZE;
@@ -132,15 +148,15 @@ uint64_t findFreeFrame(uint64_t pageIndex, uint64_t currentPageIndex = 0,
 }
 
 /**
- * @brief
- * @param pageIndex
- * @param currentPageIndex
- * @param eviction
- * @param offset
- * @param tableAddress
- * @param width
- * @param highestAddress
- * @return
+ * @brief Find the frame that holds the page we are trying to access. If there is no such frame,
+ * page in and return the new frame.
+ * @param pageIndex Index of the page we are trying to access.
+ * @param currentPageIndex The remaining digits of the pageIndex.
+ * @param eviction Whether or not we are evicting the frame we are searching for.
+ * @param offset The width of the current offset we are dealing with.
+ * @param tableAddress The address of the frame we are currently at.
+ * @param width The remaining width of the pageIndex we still need to follow.
+ * @return The (possibly newly allocated) frame that pageIndex resides in.
  */
 uint64_t findFrame(uint64_t pageIndex, uint64_t currentPageIndex, bool eviction = false,
                    size_t offset = ROOT_TABLE_OFFSET, uint64_t tableAddress = 0,
@@ -149,16 +165,21 @@ uint64_t findFrame(uint64_t pageIndex, uint64_t currentPageIndex, bool eviction 
 
     if (width == 0)
     {
+    	// If we have reached the page, return the address.
         return tableAddress;
     }
+    // Find the current index.
     word_t entry;
     uint64_t tableIndex = currentPageIndex >> (width - offset);
     uint64_t mask = tableIndex << (width - offset);
     PMread(tableAddress + tableIndex, &entry);
     if (entry)
     {
+    	// This entry exists, so follow it.
         if (eviction && width == offset)
         {
+        	// If we are trying to evict this page, and the address in this entry is the address of
+        	// the page, we should reset the entry.
             PMwrite(tableAddress + tableIndex, 0);
         }
         return findFrame(pageIndex, currentPageIndex ^ mask, eviction, OFFSET_WIDTH,
@@ -166,16 +187,20 @@ uint64_t findFrame(uint64_t pageIndex, uint64_t currentPageIndex, bool eviction 
     }
     else
     {
+    	// There is no frame holding the next table (or the page), so we need to create it.
         uint64_t nextFrame = findFreeFrame(pageIndex, 0, 0, pageIndex + 0,
                                            tableAddress);
         if (width == offset)
         {
+        	// The next frame should hold the page, so restore it from disk.
             PMrestore(nextFrame / PAGE_SIZE, pageIndex);
         }
         else
         {
+        	// The next frame will hold a table, so clear it.
             clearTable(nextFrame / PAGE_SIZE);
         }
+        // Add the next frame to this table entry, and then follow it.
         PMwrite(tableAddress + tableIndex, nextFrame / PAGE_SIZE);
         uint64_t result = findFrame(pageIndex, currentPageIndex ^ mask, eviction, OFFSET_WIDTH,
                                     nextFrame, width - offset);
@@ -201,7 +226,6 @@ int VMread(uint64_t virtualAddress, word_t *value)
 {
     if (virtualAddress >= VIRTUAL_MEMORY_SIZE)
     {
-        //TODO
         return 0;
     }
     uint64_t offset = 1LL;
@@ -227,7 +251,6 @@ int VMwrite(uint64_t virtualAddress, word_t value)
 {
     if (virtualAddress >= VIRTUAL_MEMORY_SIZE)
     {
-        //TODO
         return 0;
     }
     uint64_t offset = 1LL;
@@ -242,6 +265,7 @@ int VMwrite(uint64_t virtualAddress, word_t value)
     PMwrite(address + offset, value);
     return 1;
 }
+
 
 uint64_t evict(uint64_t victim)
 {
